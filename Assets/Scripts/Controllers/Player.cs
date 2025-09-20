d9bb0b277abf5e894b5635ac06b5a0156a0a9203
YoniGitHub200 <yoni.gabson@gmail.com>
2025-09-20 13:30:41 -0400

    Add files via upload

---
+++ Assets/Scripts/Controllers/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Transform enemyTransform;
+    public GameObject bombPrefab;
+    public List<Transform> asteroidTransforms;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 offset = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            offset += Vector3.left * 0.01f;
+        if (Input.GetKey(KeyCode.RightArrow))
+            offset += Vector3.right * 0.01f;
+        if (Input.GetKey(KeyCode.UpArrow))
+            offset += Vector3.up * 0.01f;
+        if (Input.GetKey(KeyCode.DownArrow))
+            offset += Vector3.down * 0.01f;
+
+        PlayerMovement(offset);
+    }
+
+    private void PlayerMovement(Vector3 offset)
+    {
+        transform.position = offset;
+    }
+
+}
